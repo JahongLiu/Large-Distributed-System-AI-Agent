01b742db1b537c6fe15f38e550a66d42cc82a96a
JahongLiu <jahongliu@gmail.com>
2024-10-27 15:33:19 -0400

    delete the AI example

--- ai-ebook-generation-main/api/app/ai_book_generation/sandbox.py
+++
@@ -1,6 +0,0 @@
-from docxtpl import DocxTemplate
-
-doc = DocxTemplate("templates/test.docx")
-context = {"title": "Trim Your Tummy"}
-doc.render(context)
-doc.save("generated_doc.docx")
--- ai-ebook-generation-main/api/app/health/health_service.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-
-class Health:
-    def get_health(self):
-        response = jsonify({"message": "I am healthy!"})
-        return response
